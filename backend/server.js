555432a82934c556ff896216668d621a89c1426c
JC Software Solution <vermaharshit42607@gmail.com>
2026-07-23 15:57:33 +0530

    configured backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("hii")
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/",((req,res) =>{
+    res.send("hii from backend");
+});
+
+app.listen(3000,()=>{
+    console.log("server started at 3000 port number")
+});
